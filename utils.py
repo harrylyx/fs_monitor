e5634e8e5e044b1e6c2598bcaf515d0dac322ec7
liuyixin <wj@liuyixindeMacBook-Pro.local>
2022-02-20 14:16:06 +0800

    update utils

--- utils.py
+++ utils.py
@@ -1,14 +1,17 @@
 import requests
 
-code = "601618"
-code_name = "ZGZY"
+code = "000983"
+code_name = "SXJM"
 st_dt = "2022-02-10"
 
 
-url = f"https://api.doctorxiong.club/v1/stock/kline/day?code={code}&startDate={st_dt}&type=1"
+url_kline = f"https://api.doctorxiong.club/v1/stock/kline/day?code={code}&startDate={st_dt}&type=1"
+url = f"https://api.doctorxiong.club/v1/stock?code={code}"
 
 fstr = "{0} price {1} {2} CNY"
 
+r_kline = requests.get(url_kline).json()
 r = requests.get(url).json()
-for line in r["data"]:
+for line in r_kline["data"][:-1]:
     print(fstr.format(line[0], code_name, line[2]))
+print(fstr.format(r_kline["data"][-1][0], code_name, r["data"][0]["price"]))
